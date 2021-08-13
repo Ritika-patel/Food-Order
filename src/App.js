c3d08952d0ef06fc6cb119e7e4efb7f8f68f3943
Ritika-patel <72975134+Ritika-patel@users.noreply.github.com>
2021-08-13 11:57:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Fragment, useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+  return (
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
